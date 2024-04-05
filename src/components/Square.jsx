76a71d304c890b6cc75cbde80ec84c0bc8688768
AndersonGamarraJW <andersong.v3105@gmail.com>
2024-04-05 04:13:02 +0200

    first commit

---
+++ src/components/Square.jsx
@@ -0,0 +1,9 @@
+const Square = ({ text, isActive }) => {
+  const styleActive = isActive ? "active" : "";
+  return (
+    <div className={`square ${styleActive}`}>
+      <p>{text}</p>
+    </div>
+  );
+};
+export default Square;
